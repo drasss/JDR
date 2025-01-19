5a8d6c742bef9de31b07f30238c89450bd38f9e3
drasss <aurelien640@live.fr>
2025-01-19 13:27:17 +0100

    jdr

--- dataless.py
+++ dataless.py
@@ -1,9 +1,10 @@
 import streamlit as st 
 import numpy as np
 import random
+from io import StringIO
 
 st.set_page_config(layout="wide")
-a,b,c = st.tabs(["dés", "fiche","résultats"])
+a,c,b,d = st.tabs(["dés","Fiche Personnage", "création fiche","résultats"])
 
 #lancé de dés
 lance=a.columns(3)
@@ -11,6 +12,8 @@ def dice():
     rst=[]
 
     desp=de.split("+")
+    if len(desp)==1:
+        desp+=["0"]
     des=desp[0].strip(" ").split("d")
     addd=desp[1].strip(" ")
     for i in range(int(des[0])):
@@ -22,9 +25,27 @@ de=lance[0].text_input("dé",value="2d6+5")
 lance[1].button("lancer les dés",on_click=dice)
 
 #*--------------------------- fiche de perso
+#*-------------- lecture du fichier
 
+uploaded_file=b.file_uploader("Upload Data")
+try:
+    # transformer le fichier en variable str
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    data = stringio.read()
+    #traiter le texte
+    data=data.split(",")
+    for i in range(len(data)):
+        data[i]=int(data[i])
+    extracted=True
+except :
+    data=[55]*7+[15,15,55,55,15,15]
+    extracted=False
+values=data[:7]
+#--------------- XP
+LVL=st.sidebar.number_input("Niveau",1)
+XP=st.sidebar.number_input("XP",0)
 
-values=[55]*7
+#*-------------- 7 Stats capitaux
 
     
 stats=b.columns(7)
@@ -36,21 +57,27 @@ for i in range(7):
     stat_nb+=[stats[i].number_input(name_stats[i],value=values[i],key=str(i)+"stats")]
 PV,prd,esq,PE=b.columns(4)
 
-
-prdt=(stat_nb[-1]+stat_nb[-3])/2
-prdt=int(prdt+int(prdt!=float(int(prdt))))
-esqt=(stat_nb[-2]+stat_nb[-3])/2
-esqt=int(esqt+int(esqt!=float(int(esqt))))
-pvt=int(stat_nb[0]/10)+int(stat_nb[-1]/10)+5
-pet=int(stat_nb[1]/10)+int(stat_nb[-3]/10)+5
-
+#*-------------- 4 stats secondaires
+if extracted == False:
+    prdt=(stat_nb[-1]+stat_nb[-3])/2
+    prdt=int(prdt+int(prdt!=float(int(prdt))))
+    esqt=(stat_nb[-2]+stat_nb[-3])/2
+    esqt=int(esqt+int(esqt!=float(int(esqt))))
+    pvt=int(stat_nb[0]/10)+int(stat_nb[-1]/10)+5
+    pet=int(stat_nb[1]/10)+int(stat_nb[-3]/10)+5
+    pvmax=pvt
+    pemax=pet
+else :
+    pvt,pvmax,prdt,esqt,pet,pemax=data[-6:]
 
 
 pvnb=PV.number_input("PV",value=pvt)
+pvmaxnb=PV.number_input("PV max",value=pvmax)
 prdnb=prd.number_input("PRD",value=prdt)
 esqnb=esq.number_input("ESQ",value=esqt)    
 penb=PE.number_input("PE",value=pet)
-
+pemaxnb=PE.number_input("PE max",value=pemax)
+#*-------------- Vérifications
 if np.sum(stat_nb)>55*7:
     b.text("il y a "+str(np.sum(stat_nb)-55*7)+" points en trop ")
 if np.sum(stat_nb)<55*7:
@@ -59,5 +86,22 @@ if np.min(stat_nb)<25:
     b.text("le minimum est 25")
 if np.max(stat_nb)>80:
     b.text("le minimum est 80")
+    
+#*-------------- suite
+b_xp=b.expander("Infos")
+text_fiche=b_xp.columns([3,4,2,2])
+competences=text_fiche[0].text_area("Competences")
+Inventaire=text_fiche[1].text_area("Inventaire")
+PO=text_fiche[2].number_input("PO",0)
+PA=text_fiche[3].number_input("PA",0)
+Aptitudes=b_xp.text_area("Aptitudes")
+Sorts=b_xp.text_area("Sorts")
 
+#--------------- Compagnons
+
+
+#*-------------- recolte et téléchargement des données
+data_txt=str(stat_nb).strip("[]")+", "+str([pvnb,pvmaxnb,prdnb,esqnb,penb,pemaxnb]).strip("[]") 
+
+b.download_button("Download Data",data_txt,file_name="data.txt")
 
